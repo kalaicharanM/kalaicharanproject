94a6b19e5d909f8219d2696531752c3118b2f472
kalaicharan M <kalaicharan31@gmail.com>
2022-12-12 20:27:43 +0530

    first commit

---
+++ kalaiproject/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import kalai from './components/login';
+
+function App() {
+  return (
+    <div className="App">
+      <kalai/>
+    
+    </div>
+  );
+}
+
+export default App;
